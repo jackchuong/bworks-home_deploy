8f3e2b67a40f76cd63e1daed158e08c97eb8c633
jackchuong <phamhuynhminhchuong@gmail.com>
2023-11-17 08:56:53 +0700

    add build homepage workflow

---
+++ src/pages/vi/privacy.js
@@ -0,0 +1,33 @@
+// @ts-nocheck
+import I18nProvider from 'next-translate/I18nProvider'
+import React from 'react'
+import C from '../../../src/pages_/privacy'
+import ns0 from '../../../.locales/vi/common.json'
+
+const namespaces = { 'common': ns0 }
+
+export default function Page(p){
+  return (
+    <I18nProvider 
+      lang="vi" 
+      namespaces={namespaces}  
+      internals={{"defaultLanguage":"en","isStaticMode":true}}
+    >
+      <C {...p} />
+    </I18nProvider>
+  )
+}
+
+Page = Object.assign(Page, { ...C })
+
+if(C && C.getInitialProps) {
+  Page.getInitialProps = ctx => C.getInitialProps({ ...ctx, lang: 'vi'})
+}
+
+
+
+
+
+
+
+
